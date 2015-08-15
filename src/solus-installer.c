015c239e3403cf57b9ca4008e846503a3c371e0e
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-08-15 09:28:06 -0500

    Added install button and adjusted buttonbox layout
    Added a button which will be used to trigger the Solus installation and fixed the GTK buttonbox layout so that re-sizing the window does not interfere with the buttons.

--- src/solus-installer.c
+++ src/solus-installer.c
@@ -9,20 +9,30 @@ static void quit_installer (int quit_code) {
 static void activate (GtkApplication* app, gpointer user_data) {
         GtkWidget *window;
         GtkWidget *livecd_button;
+        GtkWidget *install_button;
         GtkWidget *button_box;
 
         window = gtk_application_window_new (app);
         gtk_window_set_title (GTK_WINDOW (window), "Solus Installer");
-        gtk_window_set_resizable (GTK_WINDOW (window), TRUE);
         gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
         gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+        gtk_window_set_resizable (GTK_WINDOW (window), TRUE);
 
         button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
         gtk_container_add (GTK_CONTAINER (window), button_box);
+        gtk_button_box_set_layout (button_box, GTK_BUTTONBOX_SPREAD);
+
         livecd_button = gtk_button_new_with_label ("Keep Using LiveCD");
         g_signal_connect (livecd_button, "clicked", G_CALLBACK (quit_installer), NULL);
         g_signal_connect_swapped (livecd_button, "clicked", G_CALLBACK (gtk_widget_destroy), NULL);
+
+        install_button = gtk_button_new_with_label ("Install Solus");
+        g_signal_connect (install_button, "clicked", G_CALLBACK (quit_installer), NULL);
+        g_signal_connect_swapped (install_button, "clicked", G_CALLBACK (gtk_widget_destroy), NULL);
+
         gtk_container_add (GTK_CONTAINER (button_box), livecd_button);
+        gtk_container_add (GTK_CONTAINER (button_box), install_button);
+
         gtk_widget_show_all (window);
 }
 
