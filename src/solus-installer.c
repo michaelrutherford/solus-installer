28ce0a07011d29ed60dd2a723d93ef490181ae87
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-08-15 08:17:00 -0500

    Create solus-installer.c

---
+++ src/solus-installer.c
@@ -0,0 +1,21 @@
+#include <gtk/gtk.h>
+
+static void activate (GtkApplication* app, gpointer user_data) {
+        GtkWidget *window;
+        window = gtk_application_window_new (app);
+        gtk_window_set_title (GTK_WINDOW (window), "Solus Installer");
+        gtk_window_set_resizable (GTK_WINDOW (window), TRUE);
+        gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
+        gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+        gtk_widget_show_all (window);
+}
+
+int main (int argc, char **argv) {
+        GtkApplication *app;
+        int status;
+        app = gtk_application_new ("com.solus-project.installer", G_APPLICATION_FLAGS_NONE);
+        g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+        status = g_application_run (G_APPLICATION (app), argc, argv);
+        g_object_unref (app);
+        return status;
+}
