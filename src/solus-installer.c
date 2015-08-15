fa0ab88968d36c791f1c7cf6ce6d090fa95a1bf9
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-08-15 08:56:37 -0500

    Added "Keep Using LiveCD" Button
    Added a button that allows the user to continue using the LiveCD and not install Solus.

--- src/solus-installer.c
+++ src/solus-installer.c
@@ -2,11 +2,17 @@
 
 static void activate (GtkApplication* app, gpointer user_data) {
         GtkWidget *window;
+        GtkWidget *livecd_button;
+        GtkWidget *button_box;
         window = gtk_application_window_new (app);
         gtk_window_set_title (GTK_WINDOW (window), "Solus Installer");
         gtk_window_set_resizable (GTK_WINDOW (window), TRUE);
         gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
         gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+        button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+        gtk_container_add (GTK_CONTAINER (window), button_box);
+        livecd_button = gtk_button_new_with_label ("Keep Using LiveCD");
+        gtk_container_add (GTK_CONTAINER (button_box), livecd_button);
         gtk_widget_show_all (window);
 }
 
