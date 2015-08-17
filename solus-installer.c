54a67c83899a9af5a44c81ec49d03bb171bb70a3
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-08-16 23:56:27 -0500

    Added "what's new" button
    Added a button labeled "What's new in Solus?" that will host the latest Solus change-log.

--- solus-installer.c
+++ solus-installer.c
@@ -11,9 +11,9 @@ static void install_warning (GtkWidget* window, gpointer user_data) {
         GtkWidget *warning_text;
 
         warning_dialog = gtk_message_dialog_new (GTK_WINDOW (window), 
-                                                 GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
-                                                 GTK_MESSAGE_QUESTION,
-                                                 GTK_BUTTONS_OK_CANCEL,
+                                                 GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                 GTK_MESSAGE_WARNING,
+                                                 GTK_BUTTONS_OK,
                                                  "Installing Solus will erase the contents of the disk.");
         gtk_window_set_title (GTK_WINDOW (warning_dialog), "Warning");
         g_signal_connect_swapped (warning_dialog, "response", G_CALLBACK (gtk_widget_destroy), warning_dialog);
@@ -23,6 +23,8 @@ static void install_warning (GtkWidget* window, gpointer user_data) {
 
 static void activate (GtkApplication* app, gpointer user_data) {
         GtkWidget *window;
+        GtkWidget *new_button;
+        GtkWidget *new_icon;
         GtkWidget *livecd_button;
         GtkWidget *livecd_icon;
         GtkWidget *install_icon;
@@ -33,11 +35,10 @@ static void activate (GtkApplication* app, gpointer user_data) {
         gtk_window_set_title (GTK_WINDOW (window), "Solus Installer");
         gtk_window_set_icon_from_file (GTK_WINDOW (window), "live.png", NULL);
         gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
-        gtk_window_set_default_size (GTK_WINDOW (window), 1000, 500);
+        gtk_window_set_default_size (GTK_WINDOW (window), 1250, 500);
         gtk_window_set_resizable (GTK_WINDOW (window), TRUE);
 
         button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-        gtk_container_add (GTK_CONTAINER (window), button_box);
         gtk_button_box_set_layout (GTK_BUTTON_BOX (button_box), GTK_BUTTONBOX_SPREAD);
 
         livecd_button = gtk_button_new_with_label ("Keep Using LiveCD");
@@ -45,6 +46,12 @@ static void activate (GtkApplication* app, gpointer user_data) {
         gtk_button_set_always_show_image (GTK_BUTTON (livecd_button), TRUE);
         gtk_button_set_image (GTK_BUTTON (livecd_button), livecd_icon);
         g_signal_connect_swapped (livecd_button, "clicked", G_CALLBACK (quit_installer), window);
+        
+        new_button = gtk_button_new_with_label ("What's New In Solus?");
+        new_icon = gtk_image_new_from_file ("new.png");
+        gtk_button_set_always_show_image (GTK_BUTTON (new_button), TRUE);
+        gtk_button_set_image (GTK_BUTTON (new_button), new_icon);
+        g_signal_connect_swapped (new_button, "clicked", G_CALLBACK (quit_installer), window);
 
         install_button = gtk_button_new_with_label ("Install Solus");
         install_icon = gtk_image_new_from_file ("install.png");
@@ -52,7 +59,9 @@ static void activate (GtkApplication* app, gpointer user_data) {
         gtk_button_set_image (GTK_BUTTON (install_button), install_icon);
         g_signal_connect_swapped (install_button, "clicked", G_CALLBACK (install_warning), window);
 
+        gtk_container_add (GTK_CONTAINER (window), button_box);
         gtk_container_add (GTK_CONTAINER (button_box), livecd_button);
+        gtk_container_add (GTK_CONTAINER (button_box), new_button);
         gtk_container_add (GTK_CONTAINER (button_box), install_button);
 
         gtk_widget_show_all (window);
