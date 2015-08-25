2fdaf927c3af434e2cb73f3613ab96c12e3ec0e7
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-08-24 19:07:32 -0500

    Added gtk_notebook and removed warning
    Put welcome_button_box in a gtk_notebook and created a page for date / time and the welcome screen. Also renamed variables and restructured various elements of the project.

--- solus-installer.c
+++ solus-installer.c
@@ -1,11 +1,19 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 
+static void quit_installer (GtkWidget* window, gpointer user_data);
+static void advance_page (GtkWidget* widget, gpointer user_data);
+
 static void quit_installer (GtkWidget* window, gpointer user_data) {
         gtk_widget_destroy (window);
         exit (0);
 }
 
+static void date_time (GtkWidget* window, gpointer user_data) {
+        g_print ("Set date and time.");
+}
+
+/*
 static void install_warning (GtkWidget* window, gpointer user_data) {
         GtkWidget *warning_dialog;
         GtkWidget *warning_text;
@@ -20,6 +28,7 @@ static void install_warning (GtkWidget* window, gpointer user_data) {
 
         gtk_widget_show_all (warning_dialog);
 }
+*/
 
 static void change_log (GtkWidget* window, gpointer user_data) {
         GtkWidget *new_dialog;
@@ -41,7 +50,7 @@ static void change_log (GtkWidget* window, gpointer user_data) {
                                              "- libidn updated to 1.32 – addressing multiple concerns in older versions\n"
                                              "- kernel – addressed CVE-2015-5697\n\n"
                                              "Some obligatory mentions for other packages we also have:\n"
-                                             "- rust 1.2.0\n"
+                                             "- Rust 1.2.0\n"
                                              "- Atom 1.0.5\n"
                                              "- Telegram 0.8.48\n");
         gtk_window_set_title (GTK_WINDOW (new_dialog), "What's New?");
@@ -50,48 +59,52 @@ static void change_log (GtkWidget* window, gpointer user_data) {
         gtk_widget_show_all (new_dialog);
 }
 
-static void activate (GtkApplication* app, gpointer user_data) {
-        GtkWidget *window;
-        GtkWidget *new_button;
-        GtkWidget *new_icon;
-        GtkWidget *livecd_button;
-        GtkWidget *livecd_icon;
-        GtkWidget *install_icon;
-        GtkWidget *install_button;
-        GtkWidget *button_box;
+static void advance_page (GtkWidget* notebook, gpointer user_data) {
+        gtk_notebook_next_page (GTK_NOTEBOOK (notebook));
+}
 
-        window = gtk_application_window_new (app);
+static void activate (GtkApplication* app, gpointer user_data) {
+        GtkWidget* window = gtk_application_window_new (app);
+        GtkWidget* notebook = gtk_notebook_new ();
+        GtkWidget* new_button =gtk_button_new_with_label ("What's New In Solus?");
+        GtkWidget* new_icon = gtk_image_new_from_file ("new.png");
+        GtkWidget* livecd_button = gtk_button_new_with_label ("Keep Using LiveCD");
+        GtkWidget* livecd_icon = gtk_image_new_from_file ("live.png");
+        GtkWidget* install_icon = gtk_image_new_from_file ("install.png");
+        GtkWidget* install_button = gtk_button_new_with_label ("Install Solus");
+        GtkWidget* welcome_button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+        GtkWidget* date_time_button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+        GtkWidget* welcome_label = gtk_label_new ("Welcome");
+        GtkWidget* date_time_label = gtk_label_new ("Date/Time");
+
         gtk_window_set_title (GTK_WINDOW (window), "Solus Installer");
         gtk_window_set_icon_from_file (GTK_WINDOW (window), "live.png", NULL);
         gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
         gtk_window_set_default_size (GTK_WINDOW (window), 1250, 500);
         gtk_window_set_resizable (GTK_WINDOW (window), TRUE);
+        
+        gtk_notebook_set_tab_pos (GTK_NOTEBOOK (notebook), GTK_POS_TOP);
+        gtk_notebook_insert_page (GTK_NOTEBOOK (notebook), welcome_button_box, welcome_label, 0);
+        gtk_notebook_insert_page (GTK_NOTEBOOK (notebook), date_time_button_box, date_time_label, 1);
 
-        button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-        gtk_button_box_set_layout (GTK_BUTTON_BOX (button_box), GTK_BUTTONBOX_SPREAD);
+        gtk_button_box_set_layout (GTK_BUTTON_BOX (welcome_button_box), GTK_BUTTONBOX_SPREAD);
 
-        livecd_button = gtk_button_new_with_label ("Keep Using LiveCD");
-        livecd_icon = gtk_image_new_from_file ("live.png");
         gtk_button_set_always_show_image (GTK_BUTTON (livecd_button), TRUE);
         gtk_button_set_image (GTK_BUTTON (livecd_button), livecd_icon);
         g_signal_connect_swapped (livecd_button, "clicked", G_CALLBACK (quit_installer), window);
         
-        new_button = gtk_button_new_with_label ("What's New In Solus?");
-        new_icon = gtk_image_new_from_file ("new.png");
         gtk_button_set_always_show_image (GTK_BUTTON (new_button), TRUE);
         gtk_button_set_image (GTK_BUTTON (new_button), new_icon);
         g_signal_connect_swapped (new_button, "clicked", G_CALLBACK (change_log), window);
 
-        install_button = gtk_button_new_with_label ("Install Solus");
-        install_icon = gtk_image_new_from_file ("install.png");
         gtk_button_set_always_show_image (GTK_BUTTON (install_button), TRUE);
         gtk_button_set_image (GTK_BUTTON (install_button), install_icon);
-        g_signal_connect_swapped (install_button, "clicked", G_CALLBACK (install_warning), window);
+        g_signal_connect_swapped (install_button, "clicked", G_CALLBACK (advance_page), notebook);
 
-        gtk_container_add (GTK_CONTAINER (window), button_box);
-        gtk_container_add (GTK_CONTAINER (button_box), livecd_button);
-        gtk_container_add (GTK_CONTAINER (button_box), new_button);
-        gtk_container_add (GTK_CONTAINER (button_box), install_button);
+        gtk_container_add (GTK_CONTAINER (window), notebook);
+        gtk_container_add (GTK_CONTAINER (welcome_button_box), livecd_button);
+        gtk_container_add (GTK_CONTAINER (welcome_button_box), new_button);
+        gtk_container_add (GTK_CONTAINER (welcome_button_box), install_button);
 
         gtk_widget_show_all (window);
 }
