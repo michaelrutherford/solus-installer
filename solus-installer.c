95b6aead454a2edfca4b6941146814cc95a24196
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-10-28 15:30:57 -0500

    Updated name in license header
    Updated "Solus Installer" to "solus-installer" in license header

--- solus-installer.c
+++ solus-installer.c
@@ -1,19 +1,19 @@
 /* Copyright 2015 Michael Rutherford
 *
-* This file is part of Solus Installer.
+* This file is part of solus-installer.
 *
-* Solus Installer is free software: you can redistribute it
+* solus-installer is free software: you can redistribute it
 * and/or modify it under the terms of the GNU General Public License as
 * published by the Free Software Foundation, either version 2 of the
 * License, or (at your option) any later version.
 *
-* Solus Installer is distributed in the hope that it will be
+* solus-installer is distributed in the hope that it will be
 * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 * Public License for more details.
 *
 * You should have received a copy of the GNU General Public License along
-* with Solus Installer. If not, see http://www.gnu.org/licenses/.
+* with solus-installer. If not, see http://www.gnu.org/licenses/.
 */
 #include <gtk/gtk.h>
 #include <stdlib.h>
