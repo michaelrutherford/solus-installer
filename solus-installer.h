f702ac3dc2ee40a1c7a5b2c84e616a7ab5281db2
Michael Rutherford <michaellogan.rutherford@gmail.com>
2015-11-13 08:58:13 -0600

    Create solus-installer.h

---
+++ solus-installer.h
@@ -0,0 +1,28 @@
+/* Copyright 2015 Michael Rutherford
+*
+* This file is part of solus-installer.
+*
+* solus-installer is free software: you can redistribute it
+* and/or modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 2 of the
+* License, or (at your option) any later version.
+*
+* solus-installer is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with solus-installer. If not, see http://www.gnu.org/licenses/.
+*/
+
+#ifndef solus_installer_h
+#define solus_installer_h
+
+struct timezone {
+        char *continent[20];
+        char *location[25];
+        char *utc[10];
+};
+
+#endif
