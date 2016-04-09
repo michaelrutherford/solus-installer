35b95b14268c1aec254346fce9d9e1460580f05d
Michael Rutherford <michaellogan.rutherford@gmail.com>
2016-04-09 02:36:55 -0500

    Reformatted header

--- src/solus-installer.h
+++ src/solus-installer.h
@@ -1,20 +1,21 @@
-/* Copyright 2015-2016 Michael Rutherford
-*
-* This file is part of solus-installer.
-*
-* solus-installer is free software: you can redistribute it
-* and/or modify it under the terms of the GNU General Public License as
-* published by the Free Software Foundation, either version 2 of the
-* License, or (at your option) any later version.
-*
-* solus-installer is distributed in the hope that it will be
-* useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-* Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with solus-installer. If not, see http://www.gnu.org/licenses/.
-*/
+/* 
+ * Copyright 2015-2016 Michael Rutherford
+ *
+ * This file is part of solus-installer.
+ *
+ * solus-installer is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * solus-installer is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with solus-installer. If not, see http://www.gnu.org/licenses/.
+ */
 
 #ifndef solus_installer_h
 #define solus_installer_h
