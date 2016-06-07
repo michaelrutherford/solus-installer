c592aaa3d7e276d51cd8165fdc9a9e40186162e0
Michael Rutherford <michaellogan.rutherford@gmail.com>
2016-06-07 12:11:02 -0500

    Fixed build script

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ echo "Moving to the build directory..."
 cd build
 echo "Navigated to the build directory successfully."
 echo "Compiling solus-installer..."
-gcc `pkg-config --cflags gtk+-3.0` src/solus-installer.c src/welcome.c src/timezone.c -o solus-installer `pkg-config --libs gtk+-3.0` -Wall --std=c11
+gcc `pkg-config --cflags gtk+-3.0` ../src/solus-installer.c ../src/welcome.c ../src/timezone.c -o solus-installer `pkg-config --libs gtk+-3.0` -Wall --std=c11
 echo "Successfully compiled solus-installer."
